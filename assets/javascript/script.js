599df1151fdf93c511a591084c56abf73d79f23a
Anatude1234 <antti.korpua1@gmail.com>
2023-10-17 16:13:55 +0300

    Päivityystä edelliseen

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -17,3 +17,37 @@ document.addEventListener("DOMContentLoaded", function () {
         window.scrollTo({ top: document.body.scrollHeight, behavior: "smooth" });
     }
 });
+document.addEventListener("DOMContentLoaded", function () {
+    const passwordScreen = document.getElementById("password-screen");
+    const websiteContent = document.getElementById("website-content");
+    const passwordInput = document.getElementById("password");
+    const submitButton = document.getElementById("submit-password");
+
+    // Set your desired password and verification question
+    const correctPassword = "109";
+    const verificationQuestion = "What is the answer?";
+
+    submitButton.addEventListener("click", function () {
+        const userAnswer = passwordInput.value.trim();
+
+        if (userAnswer.toLowerCase() === correctPassword.toLowerCase()) {
+            passwordScreen.style.display = "none"; // Hide password screen
+            websiteContent.style.display = "block"; // Show website content
+        } else {
+            alert("Incorrect answer. Try again.");
+        }
+    });
+});
+document.addEventListener("DOMContentLoaded", function () {
+    // Your existing code...
+  
+    // Reference to the "Show Answer" button
+    const showAnswerButton = document.getElementById("show-answer");
+  
+    // Reference to the element displaying the correct answer
+    const correctAnswerDiv = document.getElementById("correct-answer");
+  
+    showAnswerButton.addEventListener("click", function () {
+      correctAnswerDiv.style.display = "block";
+    });
+  });
