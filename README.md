14ce8c65a1ccf3424c698f5d958f26dc234a637b
Anatude1234 <antti.korpua1@gmail.com>
2023-10-22 17:41:31 +0300

    Viimeinen päivitys

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # webbiprojekti
  
+HTML
+1 ok
+2 ok 
+3 ok
+4 ok
+5 ok
+
+CSS
+1 ok
+2 ok
+3 ok
+4 ok
+5 ok
+
+JAVASCRIPT BASICS
+1 ok
+2 ok
+3 ok
+4 -
+5 -
+
+ASYNCHRONOUS OPERATIONS
+1 -
+2 -
+3 -
+4 -
+5 -
--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,18 +1,18 @@
 document.addEventListener("DOMContentLoaded", function () {
-    // Get references to the buttons
+
     const goToTopButton = document.getElementById("goToTop");
     const goToEndButton = document.getElementById("goToEnd");
 
-    // Add click event listeners to the buttons
+   
     goToTopButton.addEventListener("click", scrollToTop);
     goToEndButton.addEventListener("click", scrollToEnd);
 
-    // Function to scroll to the top of the page
+  
     function scrollToTop() {
         window.scrollTo({ top: 0, behavior: "smooth" });
     }
 
-    // Function to scroll to the end of the page
+   
     function scrollToEnd() {
         window.scrollTo({ top: document.body.scrollHeight, behavior: "smooth" });
     }
@@ -23,7 +23,7 @@ document.addEventListener("DOMContentLoaded", function () {
     const passwordInput = document.getElementById("password");
     const submitButton = document.getElementById("submit-password");
 
-    // Set your desired password and verification question
+   
     const correctPassword = "109";
     const verificationQuestion = "What is the answer?";
 
@@ -31,20 +31,18 @@ document.addEventListener("DOMContentLoaded", function () {
         const userAnswer = passwordInput.value.trim();
 
         if (userAnswer.toLowerCase() === correctPassword.toLowerCase()) {
-            passwordScreen.style.display = "none"; // Hide password screen
-            websiteContent.style.display = "block"; // Show website content
+            passwordScreen.style.display = "none"; 
+            websiteContent.style.display = "block"; 
         } else {
             alert("Incorrect answer. Try again.");
         }
     });
 });
 document.addEventListener("DOMContentLoaded", function () {
-    // Your existing code...
-  
-    // Reference to the "Show Answer" button
+
     const showAnswerButton = document.getElementById("show-answer");
   
-    // Reference to the element displaying the correct answer
+  
     const correctAnswerDiv = document.getElementById("correct-answer");
   
     showAnswerButton.addEventListener("click", function () {
